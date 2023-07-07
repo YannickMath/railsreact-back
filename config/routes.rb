1262cc71b6bed8451f59c2d9df544de46c6b8eca
Yannick.Mathieu <yannick.mathieu@toolib.fr>
2023-07-08 01:18:56 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # ressources :users, only: [:index, :show, :update, :destroy]
+
+end
